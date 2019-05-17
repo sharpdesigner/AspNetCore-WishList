74a539f19a7f721a411c46dfc1c4db76fdbf534c
Syed Fahad <syed.muhammad.fahad@outlook.com>
2019-05-17 17:03:22 -0500

    Middleware configuration

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,13 +22,13 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment)
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler();
+                app.UseExceptionHandler("/Home/Error");
             }
             app.UseMvcWithDefaultRoute();
         }
